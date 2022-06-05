c711fbd760d2c68a88b5e284a24d383c04baaf2d
Rodrigo Augusto Vizcarra <rodrigovizcarra1996@gmail.com>
2022-06-05 14:25:29 -0300

    Creamos la funcion menorQue

--- archivo2.py
+++ archivo2.py
@@ -1,7 +1,15 @@
-from tkinter import Misc
-
-
 miSecondArchivo = "Este es mi segundo archivo"
 print(miSecondArchivo)
 print(miSecondArchivo)
 print(miSecondArchivo)
+
+def menorQue(num1,num2):
+    if num1 < num2:
+        print("El número", num1, "es menor que", num2)
+    elif num2 < num1:
+        print("El número", num2, "es menor que", num1)
+    else:
+        print("Ambos números son iguales", num1,num2)
+        
+menorQue(3,6)    
+    
